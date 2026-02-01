9dae9491f64a473e1f132a1df2c7236242b9c67d
Mike <michaelsabatino6501@gmail.com>
2026-02-01 01:33:33 -0500

    Update membership status and handle extra charges
    Changed membership status to False and added extra charges for weekend or evening shows.

--- Movie_Ticket_Calculator.py
+++ Movie_Ticket_Calculator.py
@@ -11,7 +11,7 @@ if age >= 21:
 else:
     print('User is not eligible for Evening shows')
 
-is_member = True
+is_member = False
 is_weekend = False
 
 discount = 0
@@ -21,3 +21,13 @@ if is_member and age >= 21:
 else:
     print('User does not qualify for membership discount')
 print('Discount:', discount)
+
+extra_charges = 0
+
+
+if is_weekend or show_time == 'Evening':
+    extra_charges = 2
+    print('Extra charges will be applied')
+else:
+    print("No extra charges will be applied")
+print("Extra charges:", extra_charges)
